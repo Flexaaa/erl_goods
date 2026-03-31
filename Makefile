1815f66d0ded160967984eeec5af6b2d8397ff49
aakopov <aakopov@tradingview.com>
2026-03-31 09:36:40 +0300

    Improve makefile and add request params

--- Makefile
+++ Makefile
@@ -13,6 +13,7 @@ GPROC_SRCS = $(wildcard gproc/src/*.erl)
 GPROC_OUT = gproc/ebin
 JSX_SRCS = $(wildcard jsx/src/*.erl)
 JSX_OUT = jsx/ebin
+#MAKE = make
 
 ERL_OPTS = -pa goods/ebin -pa misultin/ebin -pa gproc/ebin -pa jsx/ebin -pa cowboy/ebin -pa ranch/ebin -pa cowlib/ebin
 
@@ -21,7 +22,7 @@ all: compile
 deps:
 	$(MAKE) -C ranch
 	$(MAKE) -C cowlib
-	$(MAKE) -C cowboy
+	$(MAKE) -C cowboy ERLC_OPTS="-I $(CURDIR)/ranch/include -pa $(CURDIR)/ranch/ebin -pa $(CURDIR)/cowlib/ebin"
 
 compile: deps
 	$(ERLC) -I misultin/include -I misultin/src -o $(MISULTIN_OUT) $(MISULTIN_SRCS)
@@ -39,11 +40,14 @@ run-app: compile
 		-kernel logger_level info \
 		-eval "application:ensure_all_started(gproc), \
 		       application:ensure_all_started(inets), \
+		       application:ensure_all_started(ssl), \
 			   application:ensure_all_started(cowboy), \
 		       application:start(goods_app)."
 
 # тестировалось на локальном сервере, для этого использовал параметр перед eval
 # 		-goods_app url '"http://localhost:6666"' \
+# в случае проблем с загрузкой (роскомнадзор давит клаудфронт) и использованием средст обхода  DPI добавить следующий параметр
+#		-goods_app partial true \
 
 #Запуск тестов
 tests: compile-tests
--- goods/src/goods_requester.erl
+++ goods/src/goods_requester.erl
@@ -14,6 +14,8 @@
     timeout          :: integer(),
     url              :: string(),
     mod              :: module(),
+    partial          :: boolean(),  % true - and receiving will be by small chunks
+    chunk_size       :: integer(),  % if partial true sets number of requested elements is single request
     tref = undefined :: undefined | reference()
 }).
 
@@ -27,9 +29,11 @@ init([]) ->
     Timeout = application:get_env(goods_app, timeout, ?DEFAULT_TIMEOUT),
     Url = application:get_env(goods_app, url, ?DEFAULT_URL),
     ModName = application:get_env(goods_app, module, goods_db),
-    ?LOG_INFO("goods_requester Timeout=~p, Url=~p, ModName=~p", [Timeout,Url, ModName]),
+    Partial = application:get_env(goods_app, partial, false),
+    ChunkSize = application:get_env(goods_app, chunk_size, 10),
+    ?LOG_INFO("goods_requester Timeout=~p, Url=~p, ModName=~p, Partial =~p, ChunkSize=~p", [Timeout, Url, ModName, Partial, ChunkSize]),
     erlang:send_after(500, self(), get_data),
-    {ok, #state{timeout = Timeout, url = Url, mod = ModName}}.
+    {ok, #state{timeout = Timeout, url = Url, mod = ModName, partial = Partial, chunk_size = ChunkSize}}.
 
 handle_call({set_timeout, Timeout}, _From, State=#state{tref = TRef}) ->
     ?LOG_INFO("setting timeout ~p", [Timeout]),
@@ -47,14 +51,38 @@ handle_cast(Msg, State) ->
     {noreply, State}.
 
 
-handle_info(get_data, State=#state{timeout = Timeout, url = Url, mod = ModName}) ->
-    ?LOG_INFO("requesting data", []),    
-    case httpc:request(get, {Url, []}, [], [{body_format, binary}]) of
-        {ok, {_StatusLine, _Headers, Body}} ->
-            Entries = convert_body(Body),
-            ModName:put(Entries);
-        {error, Reason} ->
-            ?LOG_INFO("failed to get data, reason ~p", [Reason])
+handle_info(get_data, State=#state{timeout = Timeout, url = Url, mod = ModName, partial = Partial, chunk_size = ChunkSize}) ->
+    ?LOG_INFO("requesting data, URL =~p", [Url]),
+    OutHeaders = [{"User-Agent", "erl"}],
+    HTTPOptions = [{ssl, [{verify, verify_none}]}, {timeout, 3000}],
+    Options = [
+    {body_format, binary}, 
+    {socket_opts, [
+            inet, 
+            {recbuf, 65536},
+            {sndbuf, 65536},
+            {nodelay, true}
+        ]}
+    ],
+    Data = case Partial of
+        false ->
+            case httpc:request(get, {Url, OutHeaders}, HTTPOptions, Options) of
+                 {ok, {_StatusLine, _Headers, Body}} ->
+                    ?LOG_INFO("data received", []),
+                    convert_body(Body);
+                {error, Reason} ->
+                    ?LOG_INFO("failed to get data, reason ~p", [Reason]),
+                    {error, Reason}
+            end;
+        true ->
+            fetch_recursive(Url, OutHeaders, HTTPOptions, Options, ChunkSize, 0, 3, [])
+    end,
+
+    case Data of
+        {error, Reason1} ->
+            ?LOG_INFO("failed to get data, reason ~p", [Reason1]);
+        _ ->
+            ModName:put(Data)
     end,
     TRef = erlang:send_after(Timeout, self(), get_data),
     {noreply, State#state{tref = TRef}};
@@ -70,9 +98,34 @@ terminate(_Reason, _State=#state{tref = TRef}) ->
 code_change(_OldVsn, State, _Extra) ->
 	{ok, State}.
 
+fetch_recursive(Url, OutHeaders, HTTPOptions, Options, Limit, Skip, Attempts, Acc) ->
+    UrlWithParams = lists:flatten(io_lib:format(Url ++ "?limit=~p&skip=~p", [Limit, Skip])),
+    ?LOG_INFO("Requesting ~p ", [UrlWithParams]),
+    case httpc:request(get, {UrlWithParams, OutHeaders}, HTTPOptions, Options) of
+        {ok, {{_, 200, _}, _, Body}} ->
+            case convert_body(Body) of
+                [] -> 
+                    ?LOG_INFO("Готово!~n"),
+                    lists:reverse(Acc); % Конец данных
+                Products ->
+                    ?LOG_INFO("получено ~p объектов~n", [length(Products)]),
+                    % Рекурсивно запрашиваем следующую порцию
+                    fetch_recursive(Url, OutHeaders, HTTPOptions, Options, Limit, Skip + Limit, Attempts, Products ++ Acc)
+            end;
+        {error, Reason} ->
+            case Attempts of
+                0 ->
+                    {error, Reason};
+                _ ->
+                    timer:sleep(1000),
+                    ?LOG_INFO("Retrying, attempts ~p", [Attempts - 1]),
+                    fetch_recursive(Url, OutHeaders, HTTPOptions, Options, Limit, Skip, Attempts - 1, Acc)
+            end
+    end.
 
 convert_body(Body) ->
-    %?LOG_INFO("body = ~p", [Body]),
+    %?LOG_INFO("body = ~s", [Body]),
+    %?LOG_INFO("Decode = ~p", [jsx:decode(Body, [{return_maps, true}])]),
     #{<<"products">> := Products} = jsx:decode(Body, [{return_maps, true}]),
     Now = list_to_binary(calendar:system_time_to_rfc3339(erlang:system_time(second), [{offset, "Z"}])),
 
